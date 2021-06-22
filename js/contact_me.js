2bf95fecf5e8005b3e0534a4ce7f21f1960859ba
degulati <degulati@ucsd.edu>
2021-06-22 09:48:06 -0700

    [FIX] Using formspree.io to get contact me section to work with github pages

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,8 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                // url: "././mail/contact_me.php",
+                url: "https://formspree.io/f/myyljqvw",
                 type: "POST",
                 data: {
                     name: name,
@@ -26,6 +27,7 @@ $(function() {
                     email: email,
                     message: message
                 },
+                dataType: "json",
                 cache: false,
                 success: function() {
                     // Success message
